e634bf59db6bd8d992073936a73c3e8eddd882a1
Meldanor <Bhaals@gmx.de>
2013-07-02 18:31:53 +0200

    Add before and after tests

--- src/de/atp/requester/test/DateAPITest.java
+++ src/de/atp/requester/test/DateAPITest.java
@@ -50,10 +50,52 @@ public class DateAPITest extends TestCase {
         ATPDate date = new ATPDate(32, 7, 2013);
         assertTrue("Wrong day, should be 1 instead of " + date.getDay() + "(No overflow)", date.getDay() == 1);
         assertTrue("Wrong Month, should be 8 instead of " + date.getMonth() + "(No overflow)", date.getMonth() == 8);
-        
+
         date = new ATPDate(32, 12, 2013);
         assertTrue("Wrong day, should be 1 instead of " + date.getDay() + "(No overflow)", date.getDay() == 1);
         assertTrue("Wrong Month, should be 1 instead of " + date.getMonth() + "(No overflow)", date.getMonth() == 1);
         assertTrue("Wrong Year, should be 2014 instead of " + date.getYear() + "(No overflow)", date.getYear() == 2014);
+    }
+
+    public void testBeforeAndAfter() {
+
+        // Test ATPTimes
+        ATPTime firstTime = new ATPTime(12, 00, 00);
+        ATPTime secondTime = new ATPTime(13, 00, 00);
+
+        assertTrue("[TIME]: 12:00 have to be before 13:00, but wasn't!", firstTime.before(secondTime));
+        assertFalse("[TIME]: 12:00 cannot be after 13:00, but was!", firstTime.after(secondTime));
+
+        assertFalse("[TIME]: 12:00 cannot be before 12:00 (See interface specification), but was!", firstTime.after(firstTime));
+        assertFalse("[TIME]: 12:00 cannot be after 12:00 (See interface specification), but was!", firstTime.before(firstTime));
+
+        // Test ATPDates
+        ATPDate firstDate = new ATPDate(10, 2, 2013);
+        ATPDate secondDate = new ATPDate(11, 2, 2013);
+
+        assertTrue("[DATE]: 10.02.2013 have to be before 11.02.2013, but wasn't!", firstDate.before(secondDate));
+        assertFalse("[DATE]: 10.02.2013 cannot be after 11.02.2013, but was!", firstDate.after(secondDate));
+
+        assertFalse("[DATE]: 10.02.2013 cannot be before 11.02.2013 (See interface specification), but was!", firstDate.after(firstDate));
+        assertFalse("[DATE]: 10.02.2013 cannot be after 11.02.2013 (See interface specification), but was!", firstDate.before(firstDate));
+
+        // Test ATPDatetime
+        ATPDatetime firstDatetime = new ATPDatetime(10, 2, 2013, 12, 00, 00);
+        ATPDatetime secondDatetime = new ATPDatetime(10, 2, 2013, 13, 00, 00);
+
+        assertTrue("[DATETIME]: 12:00 have to be before 13:00, but wasn't!", firstDatetime.before(secondDatetime));
+        assertFalse("[DATETIME]: 12:00 cannot be after 13:00, but was!", firstDatetime.after(secondDatetime));
+
+        assertFalse("[DATETIME]: 12:00 cannot be before 12:00 (See interface specification), but was!", firstDatetime.after(firstDatetime));
+        assertFalse("[DATETIME]: 12:00 cannot be after 12:00 (See interface specification), but was!", firstDatetime.before(firstDatetime));
+
+        firstDatetime = new ATPDatetime(10, 2, 2013, 12, 00, 00);
+        secondDatetime = new ATPDatetime(11, 2, 2013, 12, 00, 00);
+
+        assertTrue("[DATETIME]: 10.02.2013 have to be before 11.02.2013, but wasn't!", firstDatetime.before(secondDatetime));
+        assertFalse("[DATETIME]: 10.02.2013 cannot be after 11.02.2013, but was!", firstDatetime.after(secondDatetime));
+
+        assertFalse("[DATETIME]: 10.02.2013 cannot be before 11.02.2013 (See interface specification), but was!", firstDatetime.after(firstDatetime));
+        assertFalse("[DATETIME]: 10.02.2013 cannot be after 11.02.2013 (See interface specification), but was!", firstDatetime.before(firstDatetime));
     }
 }
